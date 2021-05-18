d9c5504e6a6b3f7ccecc7b2e56652334cfcfabae
dizcza <dizcza@gmail.com>
2021-05-18 16:58:27 +0300

    wrong decorrelation experiment (fixed)

--- decorrelation.py
+++ decorrelation.py
@@ -1,8 +1,13 @@
+"""
+Let y1 = f(x1, W) and y2 = f(x2, W).
+Learning the weights either for (x1, y1) or (x2, y2) should decorrelate y1 and y2 signals.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import trange
 
-from kwta import kWTA, iWTA, update_weights, overlap, RESULTS_DIR
+from kwta import kWTA, iWTA, update_weights, overlap, RESULTS_DIR, kWTA_different_k
 
 N_x, N_y, N_h = 100, 200, 200
 s_x, s_w_xy, s_w_xh, s_w_hy, s_w_hh = 0.5, 0.1, 0.1, 0.1, 0.05
@@ -12,34 +17,44 @@ NUM_TO_LEARN = 5
 
 stats = {mode: np.zeros((N_REPEATS, N_ITERS), dtype=np.float32)
          for mode in ('kWTA-fixed-k', 'kWTA', 'iWTA')}
-n_active = np.zeros((N_REPEATS, N_ITERS), dtype=np.float32)
+n_active_stats = np.zeros((N_REPEATS, N_ITERS), dtype=np.float32)
+
+
+def overlap2d(tensor):
+    assert tensor.shape[1] == 2
+    return overlap(tensor[:, 0], tensor[:, 1])
+
 
 for repeat in trange(N_REPEATS):
-    x1 = np.random.binomial(1, s_x, size=N_x)
-    x2 = np.random.binomial(1, s_x, size=N_x)
+    x12 = np.random.binomial(1, s_x, size=(N_x, 2))
 
-    w_xy = np.random.binomial(1, s_w_xy, size=(N_y, N_x))
-    w_xh = np.random.binomial(1, s_w_xh, size=(N_h, N_x))
-    w_hy = np.random.binomial(1, s_w_hy, size=(N_y, N_h))
-    w_hh = np.random.binomial(1, s_w_hh, size=(N_h, N_h))
+    w_xy, w_xh, w_hy, w_hh = {}, {}, {}, {}
+    for mode in stats.keys():
+        w_xy[mode] = np.random.binomial(1, s_w_xy, size=(N_y, N_x))
+        w_xh[mode] = np.random.binomial(1, s_w_xh, size=(N_h, N_x))
+        w_hy[mode] = np.random.binomial(1, s_w_hy, size=(N_y, N_h))
+        w_hh[mode] = np.random.binomial(1, s_w_hh, size=(N_h, N_h))
 
-    for iter_id in range(100):
-        h1, y1 = iWTA(y0=w_xy @ x1, h0=w_xh @ x1, w_hy=w_hy, w_hh=w_hh)
-        h2, y2 = iWTA(y0=w_xy @ x2, h0=w_xh @ x1, w_hy=w_hy, w_hh=w_hh)
-        stats['iWTA'][repeat, iter_id] = overlap(y1, y2)
+    for iter_id in range(N_ITERS):
+        h, y = {}, {}
+        h['iWTA'], y['iWTA'] = iWTA(y0=w_xy['iWTA'] @ x12,
+                                    h0=w_xh['iWTA'] @ x12,
+                                    w_hy=w_hy['iWTA'],
+                                    w_hh=w_hh['iWTA'])
+        stats['iWTA'][repeat, iter_id] = overlap2d(y['iWTA'])
+        n_active_stats[repeat, iter_id] = np.count_nonzero(y['iWTA'], axis=0).mean()
 
-        y1_kwta_pre = w_xy @ x1 - w_hy @ (w_xh @ x1)
-        y2_kwta_pre = w_xy @ x2 - w_hy @ (w_xh @ x2)
-        overlap_kwta_fixed_k = overlap(kWTA(y1_kwta_pre, k=K_FIXED),
-                                       kWTA(y2_kwta_pre, k=K_FIXED))
-        overlap_kwta = overlap(kWTA(y1_kwta_pre, k=np.count_nonzero(y1)),
-                               kWTA(y2_kwta_pre, k=np.count_nonzero(y2)))
-        stats['kWTA-fixed-k'][repeat, iter_id] = overlap_kwta_fixed_k
-        stats['kWTA'][repeat, iter_id] = overlap_kwta
-        n_active[repeat, iter_id] = np.count_nonzero(y1)
+        for mode in ('kWTA', 'kWTA-fixed-k'):
+            h[mode] = w_xh[mode] @ x12
+            y[mode] = w_xy[mode] @ x12 - w_hy[mode] @ h[mode]
+        y['kWTA'] = kWTA_different_k(y['kWTA'], ks=np.count_nonzero(y['iWTA'], axis=0))
+        y['kWTA-fixed-k'] = kWTA(y['kWTA-fixed-k'], k=K_FIXED)
 
-        update_weights(w_hy, x_pre=h1, x_post=y1, n_choose=NUM_TO_LEARN)
-        # update_weights(w_xy, x_pre=x1, x_post=y1, n_choose=1)
+        stats['kWTA'][repeat, iter_id] = overlap2d(y['kWTA'])
+        stats['kWTA-fixed-k'][repeat, iter_id] = overlap2d(y['kWTA-fixed-k'])
+
+        for mode in stats.keys():
+            update_weights(w_hy[mode], x_pre=h[mode][:, 0], x_post=y[mode][:, 0], n_choose=NUM_TO_LEARN)
 
 colormap = {
     'iWTA': 'green',
@@ -49,8 +64,8 @@ colormap = {
 
 fig, ax = plt.subplots()
 
-n_active = n_active.mean(axis=0)
-print(f"num. of active neurons in y1: {n_active}")
+n_active_stats = n_active_stats.mean(axis=0)
+print(f"num. of active neurons in y1: {n_active_stats}")
 for key in stats.keys():
     mean = stats[key].mean(axis=0)
     std = stats[key].std(axis=0)
@@ -59,7 +74,7 @@ for key in stats.keys():
                     facecolor=colormap[key], alpha=0.3)
 ax.set_title(f"Decorrelation. num_to_learn={NUM_TO_LEARN}")
 ax.legend()
-ax.set_xlabel('iteration')
-ax.set_ylabel('overlap(y1, y2)')
+ax.set_xlabel('Iteration')
+ax.set_ylabel('Overlap($y_1$, $y_2$)')
 plt.savefig(RESULTS_DIR / "decorrelation.jpg")
 # plt.show()
